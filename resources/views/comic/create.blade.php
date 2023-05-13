9d8b1de491b1b6d5237e2782916016d1230953cc
Th4Dude <gallottimaurizio@yahoo.it>
2023-05-13 13:50:55 +0300

    aggiunta old in create

--- resources/views/comic/create.blade.php
+++ resources/views/comic/create.blade.php
@@ -34,41 +34,41 @@ Creazione
         @csrf
         <div class="mb-3">
           <label for="src" class="form-label">Immagine</label>
-          <input type="text" class="form-control" id="src" name="thumb"> 
+          <input type="text" class="form-control" id="src" name="thumb" value="{{ old('thumb') }}"> 
         </div>
 
         <div class="mb-3">
             <label for="descrizione" class="form-label">Descrizione</label>
-            <textarea class="form-control" id="descrizione" rows="2" name="description"></textarea>
+            <textarea class="form-control" id="descrizione" rows="2" name="description" value="{{ old('description') }}"></textarea>
         </div>
         <div class="mb-3">
             <label for="titolo" class="form-label">Titolo</label>
-            <input type="text" class="form-control" id="titolo" name="title">
+            <input type="text" class="form-control" id="titolo" name="title" value="{{ old('title') }}">
         </div>
 
         <div class="mb-3">
             <label for="prezzo" class="form-label">Prezzo</label>
-            <input type="number" class="form-control" id="prezzo" name="price" min="5" max="20">
+            <input type="number" class="form-control" id="prezzo" name="price" min="5" max="20" value="{{ old('price') }}">
         </div>
         <div class="mb-3">
             <label for="serie" class="form-label">Serie</label>
-            <input type="text" class="form-control" id="serie" name="series">
+            <input type="text" class="form-control" id="serie" name="series" value="{{ old('series') }}">
         </div>
         <div class="mb-3">
             <label for="data" class="form-label">Data di uscita</label>
-            <input type="date" class="form-control" id="data" name="sale_date">
+            <input type="date" class="form-control" id="data" name="sale_date" value="{{ old('sale_date') }}">
         </div>
         <div class="mb-3">
             <label for="serie" class="form-label">Genere</label>
-            <input type="text" class="form-control" id="serie" name="type">
+            <input type="text" class="form-control" id="serie" name="type" value="{{ old('type') }}">
         </div>
         <div class="mb-3">
             <label for="artists" class="form-label">Artisti</label>
-            <textarea class="form-control" id="artists" rows="2" name="artists"></textarea>
+            <textarea class="form-control" id="artists" rows="2" name="artists">{{ old('artists') }}</textarea>
         </div>
         <div class="mb-3">
             <label for="writers" class="form-label">Scrittori</label>
-            <textarea class="form-control" id="writers" rows="2" name="writers"></textarea>
+            <textarea class="form-control" id="writers" rows="2" name="writers">{{ old('writers') }}</textarea>
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
       </form>
